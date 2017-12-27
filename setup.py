a950188177aedc19d5f5f132f9f1325a347a904c
Vikctar <vikc.steve@gmail.com>
2017-12-27 19:04:56 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='listOfLists',
+    version='1.0.0',
+    py_modules=['listOfLists'],
+    author='Vikctar',
+    author_email='vikc.steve@gmail.com',
+    url='https://github.com/Vikctar/list-of-lists',
+    description='A simple printer of lists and nested lists'
+
+)
